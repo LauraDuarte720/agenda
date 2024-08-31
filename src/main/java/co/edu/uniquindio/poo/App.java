ff57775f410faf64805ecdc97439682ce55fe713
Laurac <laurac.duarteq@uqvirtual.edu.co>
2024-08-31 17:26:44 -0500

    error fecha

--- src/main/java/co/edu/uniquindio/poo/App.java
+++ src/main/java/co/edu/uniquindio/poo/App.java
@@ -9,9 +9,9 @@ import java.time.LocalTime;
  */
 public class App {
     public static void main(String[] args) {
-        Grupo grupo=new Grupo("cumpelanos", Categoria.FIESTA);
+        Grupo grupo=new Grupo("cumpelaños", Categoria.FIESTA);
         Grupo grupo2=new Grupo ("Cita",Categoria.FAMILIA);
-        Reunion reunion =new Reunion("Trabajo",LocalDate.of(2024, 30, 4) , LocalTime.of(2, 00, 00));
+        Reunion reunion =new Reunion("Trabajo",LocalDate.of(2024, 2, 4) , LocalTime.of(2, 00, 00));
         Reunion reunion2 =new Reunion("Fiesta",LocalDate.of(2024, 11, 9) , LocalTime.of(4, 30, 00));
         Contacto contacto=new Contacto("Juliana", "Juli", "7 de Agosto", "222","jv@gmail.com" );
         Contacto contacto2=new Contacto("Sara", "Saris", "Calarca", "555","sb@gmail.com" );
