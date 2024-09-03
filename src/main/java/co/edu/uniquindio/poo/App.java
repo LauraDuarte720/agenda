d18c8a3752cf778df41957794961a69dc4226c04
Laurac <laurac.duarteq@uqvirtual.edu.co>
2024-09-02 20:25:15 -0500

    grupo

--- src/main/java/co/edu/uniquindio/poo/App.java
+++ src/main/java/co/edu/uniquindio/poo/App.java
@@ -9,22 +9,20 @@ import java.time.LocalTime;
 public class App {
     public static void main(String[] args) {
         Grupo grupo=new Grupo("cumpelaños", Categoria.FIESTA);
-        Grupo grupo2=new Grupo ("Cita",Categoria.FAMILIA);
         Reunion reunion =new Reunion("Trabajo",LocalDate.of(2024, 2, 4) , LocalTime.of(2, 00, 00));
         Contacto contacto=new Contacto("Juliana", "Juli", "7 de Agosto", "222","jv@gmail.com" );
         Contacto contacto2=new Contacto("Sara", "Saris", "Calarca", "555","sb@gmail.com" );
         Contacto contacto3=new Contacto("Juan", "Juanitio", "El  paraiso", "467", "jj@gmail.com");
         Contacto contacto4=new Contacto("Mariana", "Mari", "El granada", "888", "mm@gmail.com");
         Contacto contacto5=new Contacto("Jacobo", "Jaco", "La aurora", "777", "jb@gmail.com");
-        grupo.agregarContacto(contacto);
-        grupo2.agregarContacto(contacto);
         Agenda.mostrarMensaje(grupo.toString());
-        Agenda.mostrarMensaje(grupo2.toString());
-        reunion.agregarContacto(contacto);
-        reunion.agregarContacto(contacto2);
-        reunion.agregarContacto(contacto3);
-        reunion.agregarContacto(contacto4);
+        grupo.agregarContacto(contacto);
+        grupo.agregarContacto(contacto2);
+        grupo.agregarContacto(contacto3);
+        grupo.agregarContacto(contacto4);
+        grupo.agregarContacto(contacto5);
         reunion.agregarContacto(contacto5);
+        reunion.agregarContacto(contacto4);
         Agenda.mostrarMensaje(reunion.toString());
         reunion.removerContactoAsistente("Juliana", "222");
         Agenda.mostrarMensaje(reunion.toString());
@@ -32,7 +30,6 @@ public class App {
         Agenda agenda =new Agenda("Agenda Personal");
         agenda.agregarContactoAgenda(contacto);
         agenda.agregarContactoAgenda(contacto2);
-        agenda.agregarGrupo(grupo2);
         agenda.agregarGrupo(grupo);
         agenda.agregarreuniones(reunion);
         Agenda.mostrarMensaje(agenda.toString());
